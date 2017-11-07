8e7feae2076e794df10823585a05b88ec42d026d
xuyanwei2016 <503142833@qq.com>
2017-11-07 18:51:49 +0800

    创建一个react文件

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+let path=require('path');
+let HtmlWebpackPlugin=require('html-webpack-plugin');
+module.exports={
+    entry:'./src/main.js',
+    output:{
+        filename:'bundle.js',
+        path:path.resolve('./dist'),
+    },
+    module:{
+        rules:[
+            {test:/\.js$/,use:'babel-loader',exclude:/node_modules/},
+            {test:/\.less$/,use:['style-loader','css-loader','less-loader']},
+            {test:/\.(jpg|png|gif)$/,use:'url-loader'}
+        ]
+    },
+    devtool:'source-map',
+    plugins:[
+        new HtmlWebpackPlugin({
+            template:'./index.html'//告诉以哪个模板打包 把所有文件都打包到这个html中
+        })
+    ]
+}
+
+
+
